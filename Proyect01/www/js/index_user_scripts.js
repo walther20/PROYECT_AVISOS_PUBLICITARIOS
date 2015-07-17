7097b86b414dbe99932ec23aef85b99588719ec3
walther20 <waltherccos@gmail.com>
2015-07-17 14:10:14 -0500

    primer commit001

---
+++ Proyect01/www/js/index_user_scripts.js
@@ -0,0 +1,19 @@
+(function()
+{
+ "use strict";
+ /*
+   hook up event handlers 
+ */
+ function register_event_handlers()
+ {
+    
+    
+     /* button  INGRESAR */
+    
+    
+        /* button  INGRESAR */
+    
+    
+    }
+ document.addEventListener("app.Ready", register_event_handlers, false);
+})();
